e3830b7fb65b465e37d2005e989fe3173d310af8
carlosivanxd <carlosivanxd5@gmail.com>
2024-10-20 17:44:46 -0600

    Plantilla Agregada

--- api/login/login_view.py
+++ api/login/login_view.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_views(request):
-    template_name = "login.html"
+    template_name = "auth-login.html"
     return render(request, template_name)
